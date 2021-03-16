3bf2cb885ccbdd6b1673ea449baa39ca5bb3494a
Simon Law <simonlaw910@gmail.com>
2021-03-16 21:46:54 +0800

    update files

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -6,7 +6,7 @@ const htmlModules = require('./themeConfig/htmlModules.js');
 module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-  logo: '/img/EB-logo.png', // 导航栏logo
+  logo: '/img/logo.png', // 导航栏logo
   repo: 'realsimonooo', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
@@ -52,7 +52,7 @@ module.exports = {
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
-    avatar: 'https://avatars.githubusercontent.com/u/60037549?s=460&u=701343ec421afbefb8a0e524cefbdb0b37743364&v=4',
+    avatar: 'img/avastar.jpg',
     name: 'Simon Law',
     slogan: '还要努努力',
   },
